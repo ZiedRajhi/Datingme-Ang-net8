f33e8b2cdbd0bf4b77f17edd3ad0a4f552283a93
Zied RAJHI <zied.rajhi@ecostaff.fr>
2025-01-12 22:40:23 +0100

    Authentification basic DTO +JWT

---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace API.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentity(this IServiceCollection services, IConfiguration config) 
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            var tokenKey = config["TokenKey"] ?? throw new Exception("TokenKey not found");
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey))
+            };
+        });
+            return services;    
+        }
+    }
+}
