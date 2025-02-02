c7175b5b26475c9a996164e434f528ad6580c042
Zied RAJHI <zied.rajhi@ecostaff.fr>
2025-02-02 16:44:09 +0100

    Error handling

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -5,8 +5,8 @@ namespace API.DTOs
     public class RegisterDto
     {
         [Required]
-        [MaxLength(100)]
-        public required string Username { get; set; }
+        
+        public  string Username { get; set; }
 
         [Required]
         public required string Password { get; set; }
